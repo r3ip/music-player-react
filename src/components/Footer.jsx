83ff2ce341826cb7fb8a29f267cfc3916cee7a0e
Piero Valenzuela <piero.valenzuela@jycleadingcorp.com>
2023-04-04 16:52:48 -0500

    update components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import {AiOutlineGithub} from 'react-icons/all'
 export const Footer = () => {
   return (
     <footer className="footer footer-center p-4">
-          <div>
-           <p>Creado por: <strong>Pier Valenzuela</strong> <a href="https://github.com/r3ip/" target={"_blank"}><AiOutlineGithub className="inline w-6 h-6"/></a></p>
-          </div>
+      <div>
+        <p>Creado por: <strong>Pier Valenzuela</strong> <a href="https://github.com/r3ip/" target={"_blank"}><AiOutlineGithub className="inline w-6 h-6"/></a></p>
+      </div>
     </footer>
   )
 }
